4be94caf5abb448930e2d263677a7e51031159f7
evs-ptr <evs.ptr@proton.me>
2025-07-07 13:00:12 +0500

    Init `ServerMain.svelte`

--- src/routes/rcon/+page.svelte
+++ src/routes/rcon/+page.svelte
@@ -1,40 +1,17 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte'
-	import { createServersManager } from './servers-manager.svelte'
-	import { RustRconConnection } from './rust-rcon'
-	import ServersChooser from './ServersChooser.svelte'
 	import type { RustServer } from './rust-server.svelte'
+	import ServerMain from './ServerMain.svelte'
+	import { createServersManager } from './servers-manager.svelte'
+	import ServersChooser from './ServersChooser.svelte'
 
 	const serversManager = createServersManager()
 	let selectedServer = $state<RustServer | null>(null)
-
-	// let rcon: RustRconConnection = null!
-
-	// let inputUrl = $state('ws://localhost:24247/1jEIXkbSQty3')
-	// let inputPassword = $state('1jEIXkbSQty3')
-
-	// async function connect() {
-	// 	rcon = new RustRconConnection(inputUrl)
-	// 	await rcon.connect()
-	// }
-
-	// onMount(async () => {})
-
-	// onDestroy(() => {
-	// 	rcon?.disconnect()
-	// 	rcon = null!
-	// })
 </script>
 
 <div>
 	<h1>RCON</h1>
 	<ServersChooser {serversManager} bind:selectedServer />
 	{#if selectedServer}
-		<div>
-			<h2>Server: {selectedServer.ipPort}</h2>
-		</div>
+		<ServerMain server={selectedServer} />
 	{/if}
-	<!-- <input class="dark:bg-zinc-700" type="text" bind:value={inputUrl} placeholder="URL" />
-	<input type="text" bind:value={inputPassword} placeholder="Password" />
-	<button class="" disabled={!inputUrl || !inputPassword} onclick={connect}>Connect</button> -->
 </div>
---
+++ src/routes/rcon/ServerMain.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { RustServer } from './rust-server.svelte'
+
+	interface Props {
+		server: RustServer
+	}
+
+	let { server }: Props = $props()
+</script>
+
+<div>
+	<h2>Server: {server.ipPort}</h2>
+</div>
