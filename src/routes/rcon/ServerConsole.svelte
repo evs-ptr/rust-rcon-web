6766304d2d70694e56efb93a80a14925877512e9
evs-ptr <evs.ptr@proton.me>
2025-07-08 12:46:27 +0500

    `ServerConsole` scroll on update when near bottom

--- src/routes/rcon/ServerConsole.svelte
+++ src/routes/rcon/ServerConsole.svelte
@@ -13,6 +13,32 @@
 	const SUBSCRIBE_ID = 'cnsle'
 	let unsubscribe: (() => void) | null = null
 
+	let consoleContainer: HTMLDivElement | undefined
+	const SCROLL_THRESHOLD = 16 * 2.5
+	let shouldScroll = false
+
+	$effect.pre(() => {
+		// This is needed to make sure that the effect is triggered on messages change
+		store.messages.length
+
+		if (!consoleContainer) {
+			return
+		}
+
+		shouldScroll =
+			consoleContainer.scrollHeight - consoleContainer.clientHeight - consoleContainer.scrollTop <
+			SCROLL_THRESHOLD
+	})
+
+	$effect(() => {
+		// This is needed to make sure that the effect is triggered on messages change
+		store.messages.length
+
+		if (shouldScroll && consoleContainer) {
+			consoleContainer.scrollTop = consoleContainer.scrollHeight
+		}
+	})
+
 	function onMessage(msg: CommandResponse) {
 		console.log(msg)
 		store.messages.push(msg.Message)
@@ -42,7 +68,10 @@
 </script>
 
 <div>
-	<div class="flex h-96 flex-col gap-2 overflow-x-scroll font-mono text-xs">
+	<div
+		bind:this={consoleContainer}
+		class="flex h-96 flex-col gap-2 overflow-y-scroll font-mono text-xs"
+	>
 		{#each store.messages as message}
 			<div>
 				<span class="overflow-x-scroll text-nowrap">{message}</span>
@@ -51,10 +80,12 @@
 	</div>
 	<div>
 		<form onsubmit={handleSubmit} class="flex gap-2">
+			<!-- svelte-ignore a11y_autofocus -->
 			<input
-				type="text"
 				bind:value={store.commandInput}
+				type="text"
 				class="flex-grow rounded border bg-transparent p-2"
+				autofocus
 			/>
 			<button type="submit">Send</button>
 		</form>
