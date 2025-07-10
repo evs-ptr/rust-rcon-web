c8887cc45dbaacb99dc9794b6ad2a40752aa3a47
evs-ptr <evs.ptr@proton.me>
2025-07-10 15:38:20 +0500

    `ServerConnect` use shadcn card

--- src/routes/rcon/ServerConnect.svelte
+++ src/routes/rcon/ServerConnect.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button'
+	import * as Card from '$lib/components/ui/card/index'
 	import { Input } from '$lib/components/ui/input'
 	import { Label } from '$lib/components/ui/label'
 	import type { RustServer } from './rust-server.svelte'
@@ -15,28 +16,32 @@
 	}
 </script>
 
-<form onsubmit={tryConnect} class="flex w-full max-w-sm flex-col gap-4 self-center">
-	<div class="grid gap-2">
-		<Label for="server-address">Server Address</Label>
-		<Input
-			id="server-address"
-			type="text"
-			placeholder="IP:Port"
-			autocomplete="off"
-			bind:value={server.ipPort}
-		/>
-	</div>
+<Card.Root class="w-full max-w-sm self-center">
+	<Card.Content>
+		<form onsubmit={tryConnect} class="flex flex-col gap-6">
+			<div class="grid gap-2">
+				<Label for="server-address">Server Address</Label>
+				<Input
+					id="server-address"
+					type="text"
+					placeholder="IP:Port"
+					autocomplete="off"
+					bind:value={server.ipPort}
+				/>
+			</div>
 
-	<div class="grid gap-2">
-		<Label for="server-password">RCON Password</Label>
-		<Input
-			id="server-password"
-			type="password"
-			placeholder="Password"
-			autocomplete="off"
-			bind:value={server.password}
-		/>
-	</div>
+			<div class="grid gap-2">
+				<Label for="server-password">RCON Password</Label>
+				<Input
+					id="server-password"
+					type="password"
+					placeholder="Password"
+					autocomplete="off"
+					bind:value={server.password}
+				/>
+			</div>
 
-	<Button type="submit">Connect</Button>
-</form>
+			<Button type="submit">Connect</Button>
+		</form>
+	</Card.Content>
+</Card.Root>
