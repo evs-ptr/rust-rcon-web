a5372969d8cbf2cd58010ebee3752bea625e3282
evs-ptr <evs.ptr@proton.me>
2025-07-14 16:06:43 +0500

    `config-global` initial settings

--- src/lib/config-global.svelte.ts
+++ src/lib/config-global.svelte.ts
@@ -6,10 +6,20 @@ const DEFAULT_CONTEXT_KEY = 'configGlobal'
 
 type ConfigGlobalJson = {
 	version: number
+	consoleHistoryFetch: number
+	consoleHistoryClamp: number
+	consoleChatInclude: boolean
+	consoleChatHistoryFetch: number
 }
 
 export class ConfigGlobal extends StorageSyncedState {
 	version: number = 1
+
+	consoleHistoryFetch: number = 300
+	consoleHistoryClamp: number = 3_000
+
+	consoleChatInclude: boolean = true
+	consoleChatHistoryFetch: number = 90
 
 	constructor() {
 		super(STORAGE_KEY)
@@ -20,11 +30,27 @@ export class ConfigGlobal extends StorageSyncedState {
 		if ('version' in json && typeof json.version === 'number') {
 			this.version = json.version
 		}
+		if ('consoleHistoryFetch' in json && typeof json.consoleHistoryFetch === 'number') {
+			this.consoleHistoryFetch = json.consoleHistoryFetch
+		}
+		if ('consoleHistoryClamp' in json && typeof json.consoleHistoryClamp === 'number') {
+			this.consoleHistoryClamp = json.consoleHistoryClamp
+		}
+		if ('consoleChatInclude' in json && typeof json.consoleChatInclude === 'boolean') {
+			this.consoleChatInclude = json.consoleChatInclude
+		}
+		if ('consoleChatHistoryFetch' in json && typeof json.consoleChatHistoryFetch === 'number') {
+			this.consoleChatHistoryFetch = json.consoleChatHistoryFetch
+		}
 	}
 
 	toJSON(): ConfigGlobalJson {
 		return {
 			version: this.version,
+			consoleHistoryFetch: this.consoleHistoryFetch,
+			consoleHistoryClamp: this.consoleHistoryClamp,
+			consoleChatInclude: this.consoleChatInclude,
+			consoleChatHistoryFetch: this.consoleChatHistoryFetch,
 		}
 	}
 }
