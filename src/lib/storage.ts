d29555b83eb528963d2ea2c84987da371cae6d02
evs-ptr <evs.ptr@proton.me>
2025-07-14 10:27:30 +0500

    Init `storage.ts`

---
+++ src/lib/storage.ts
@@ -0,0 +1,42 @@
+import { browser } from '$app/environment'
+
+export function saveToStorage(key: string, obj: object): void {
+	if (!browser) {
+		return
+	}
+
+	try {
+		const json = JSON.stringify(obj)
+		localStorage.setItem(key, json)
+	} catch (e) {
+		console.error('Error while saveToStorage', e)
+	}
+}
+
+export function getFromStorage<T extends object>(key: string): T | null {
+	if (!browser) {
+		return null
+	}
+
+	const json = localStorage.getItem(key)
+	if (json === null) {
+		return null
+	}
+
+	try {
+		const obj = JSON.parse(json) as T
+		return obj
+	} catch (e) {
+		console.error('Error while parsing json getFromStorage', e)
+	}
+
+	return null
+}
+
+export function deleteFromStorage(key: string) {
+	if (!browser) {
+		return
+	}
+
+	localStorage.removeItem(key)
+}
