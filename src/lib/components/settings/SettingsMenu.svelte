5c666ecb845ec0f51ae7204e38de563688e83fe5
evs-ptr <evs.ptr@proton.me>
2025-07-15 11:44:32 +0500

    `SettingsMenu` init

---
+++ src/lib/components/settings/SettingsMenu.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js'
+	import { Input } from '$lib/components/ui/input/index.js'
+	import { Label } from '$lib/components/ui/label/index.js'
+	import * as Sheet from '$lib/components/ui/sheet/index.js'
+	import { getConfigGlobalContext } from '$lib/config-global.svelte'
+	import SettingsIcon from '@lucide/svelte/icons/settings'
+
+	const config = getConfigGlobalContext()
+
+	function saveConfig() {
+		config.save()
+	}
+</script>
+
+<Sheet.Root>
+	<Sheet.Trigger class={buttonVariants({ variant: 'outline' })}>
+		<SettingsIcon />
+	</Sheet.Trigger>
+	<Sheet.Content>
+		<Sheet.Header>
+			<Sheet.Title>Global Settings</Sheet.Title>
+		</Sheet.Header>
+		<div class="grid flex-1 auto-rows-min gap-6 overflow-scroll px-4">
+			<div class="grid gap-3">
+				<Label for="name" class="text-right">Name</Label>
+				<Input id="name" value="Pedro Duarte" />
+			</div>
+		</div>
+		<Sheet.Footer class="flex flex-col gap-4 sm:flex-row sm:gap-2">
+			<Button variant="outline">Reset</Button>
+			<Sheet.Close class={[buttonVariants({ variant: 'default' }), 'grow']} onclick={saveConfig}
+				>Save Settings to Storage</Sheet.Close
+			>
+		</Sheet.Footer>
+	</Sheet.Content>
+</Sheet.Root>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state'
+	import SettingsMenu from '$lib/components/settings/SettingsMenu.svelte'
 	import { Button } from '$lib/components/ui/button/index.js'
 	import MoonIcon from '@lucide/svelte/icons/moon'
 	import SunIcon from '@lucide/svelte/icons/sun'
@@ -24,7 +25,8 @@
 				RCon
 			</Button>
 		</nav>
-		<div>
+		<div class="flex flex-row gap-2">
+			<SettingsMenu />
 			<Button onclick={toggleMode} variant="outline" size="icon">
 				<SunIcon
 					class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
