c9adf16586916c2782356be6b43523f9cdbdd5c5
evs-ptr <evs.ptr@proton.me>
2025-08-28 11:52:21 +0500

    `+layout` increase x padding on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import '../app.css'
 
-	import { onDestroy } from 'svelte'
+	import { setConfigGlobalContext } from '$lib/config-global.svelte'
+	import { setConfigStateContext } from '$lib/config-state.svelte'
 	import { ModeWatcher } from 'mode-watcher'
+	import { onDestroy } from 'svelte'
 	import Footer from './Footer.svelte'
 	import Header from './Header.svelte'
 	import { setServersManagerContext } from './rcon/servers-manager.svelte'
-	import { setConfigStateContext } from '$lib/config-state.svelte'
-	import { setConfigGlobalContext } from '$lib/config-global.svelte'
 
 	let { children } = $props()
 
@@ -26,7 +26,7 @@
 <div class="flex min-h-screen flex-col">
 	<Header />
 
-	<main class="container mx-auto flex flex-1 flex-col overflow-clip px-2 py-4 sm:px-4 lg:px-8">
+	<main class="container mx-auto flex flex-1 flex-col overflow-clip px-4 py-4 sm:px-6 lg:px-8">
 		{@render children()}
 	</main>
 
