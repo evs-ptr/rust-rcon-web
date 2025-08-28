82b86b73eed6d7cac9f2c5f043a23e65c7373b03
evs-ptr <evs.ptr@proton.me>
2025-08-28 09:34:05 +0500

    `monaco.ts` exclude `editor.worker` from bundle for now
    As it was not used anywhere

--- src/routes/rcon/monaco.ts
+++ src/routes/rcon/monaco.ts
@@ -1,16 +1,19 @@
 import * as monaco from 'monaco-editor'
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
 
 self.MonacoEnvironment = {
-	getWorker: function (_: string, label: string) {
-		switch (label) {
-			case 'json':
-				return new jsonWorker()
-			default:
-				return new editorWorker()
-		}
+	getWorker: function () {
+		return new jsonWorker()
 	},
+	// getWorker: function (_: string, label: string) {
+	// 	return jsonWorker()
+	// 	switch (label) {
+	// 		case 'json':
+	// 			return new jsonWorker()
+	// 		default:
+	// 			return new editorWorker()
+	// 	}
+	// },
 }
 
 export default monaco
